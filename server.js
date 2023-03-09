5eedf8f8744a6a9cb3a7335db83e048058993e50
Kim <73111472+shakiraluthfiania@users.noreply.github.com>
2023-03-09 14:48:40 +0700

    Change Content

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+	res.send('Cheer Up my baby!');
 });
 
 const PORT = process.env.PORT || 8080;
